095b37905cf901dae2c96a647411feef96e95b0d
uimm258 <uimm258@gmail.com>
2020-09-22 08:49:50 -0700

    ..

--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ import './index.css';
 
 import shoppingList from './shopping-list';
 
+
 const main = function () {
+  fetch('https://thinkful-list-api.herokuapp.com/ei-student/items')
+  .then(res => res.json())
+  .then(data => console.log(data));
+
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
